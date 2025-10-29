c3679022fca88b695bd9344dc329ba47c6af6d27
Willyan <willyanalvesgomes@hotmail.com>
2025-10-29 10:53:18 -0300

    iniciando cadastro de funcionarios

---
+++ cadastrar-funcionario.php
@@ -0,0 +1,27 @@
+<h1>Cadastrar Funcionário</h1>
+<form action="?page=salvar-funcionario" method="POST">
+    <input type="hidden" name="acao" value="cadastrar">
+    <div class="mb-3">
+        <label>Nome
+            <input type="text" name="nome-funcionario" class="form-control">
+        </label>
+    </div>
+    <div class="mb-3">
+        <label>Email
+            <input type="email" name="email-funcionario" class="form-control">
+        </label>
+    </div>
+    <div class="mb-3">
+        <label>CPF
+            <input type="text" name="cpf-funcionario" class="form-control">
+        </label>
+    </div>
+    <div class="mb-3">
+        <label>Telefone
+            <input type="text" name="telefone-funcionario" class="form-control">
+        </label>
+    </div>
+    <div>
+        <button type="submit" class="btn btn-primary">Enviar</button>
+    </div>
+</form>
---
+++ salvar-funcionario.php
@@ -0,0 +1,36 @@
+<?php
+    switch ($_REQUEST['acao']) {
+        case 'cadastrar':
+            $nome = $_POST['nome_funcionario']
+            $email = $_POST['email_funcionario']
+            $cpf = $_POST['cpf_funcionario']
+            $fone = $_POST['fone_funcionario']
+    }
+
+    $sql = "INSERT INTO funcionario (
+                nome_funcionario, cpf_funcionario, email_funcionario,
+                    fone_funcionario
+            VALUES ('{$nome}', '{$cpf}', '{$email}', '{$fone}')";
+        
+    $res = $conn->query($sql);
+
+    if($res == true){
+        print "<script>alert('Cadastrou com sucesso!');</script>"
+        print "<script>location.href='?page=listar-funcionario';</script>"
+    }else{
+        print "<script>alert('Não cadastrou!');</script>"
+        print "<script>location.href='?page=listar-funcionario';</script>"
+
+    }
+    break;
+
+    case 'editar':
+        //code...
+        break;
+    case 'excluir':
+        //code...
+        break;
+    default:
+        //code...
+        break;
+?>
