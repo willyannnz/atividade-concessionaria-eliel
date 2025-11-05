4c47145bf50f33d2b8c9e46a836e367609aaffb9
Willyan <willyanalvesgomes@hotmail.com>
2025-11-05 09:22:59 -0300

    corrige formatação e adiciona nova linha no final do arquivo salvar-funcionario.php

--- salvar-funcionario.php
+++ salvar-funcionario.php
@@ -1,14 +1,14 @@
 <?php
 
     include('config.php');
-    
+
     switch ($_REQUEST['acao']) {
         case 'cadastrar':
             $nome = $_POST['nome_funcionario'];
             $email = $_POST['email_funcionario'];
             $cpf = $_POST['cpf_funcionario'];
             $fone = $_POST['fone_funcionario'];
-    }
+    
 
             $sql = "INSERT INTO funcionario (
                         nome_funcionario, 
@@ -38,4 +38,5 @@
         default:
             //code...
             break;
+    }
 ?>
