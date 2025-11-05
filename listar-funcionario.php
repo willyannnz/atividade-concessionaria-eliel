9d0bc29f166320ffb079104d214915fcd4f8c4b0
Willyan <willyanalvesgomes@hotmail.com>
2025-11-05 20:50:58 -0300

    corrige erros de exibição na listagem de funcionários

--- listar-funcionario.php
+++ listar-funcionario.php
@@ -8,16 +8,14 @@
     $qtd = $res->num_rows;
 
     if($qtd > 0){
-        // Erro 1 (;) e Erro 4 (</b>) corrigidos aqui
         print "<p>Encontrou <b>$qtd</b> resultado(s)</p>";
-        
-        // Erro 1 (;) corrigido aqui
         print "<table class='table table-bordered table-striped table-hover'>";
         print "<tr>";
         print "<th>#</th>";
         print "<th>Nome</th>";
         print "<th>Email</th>";
-        print "<th>Telefone</th>"; // Header
+        print "<th>Telefone</th>"; 
+        print "<th>CPF</th>"; 
         print "</tr>";
         
         while($row = $res->fetch_object()){
@@ -25,18 +23,14 @@
             print "<td>".$row->id_funcionario."</td>";     
             print "<td>".$row->nome_funcionario."</td>";     
             print "<td>".$row->email_funcionario."</td>";     
-            
-            // Erro 2 (nome da coluna) corrigido aqui
+            print "<td>".$row->cpf_funcionario."</td>";
             print "<td>".$row->fone_funcionario."</td>";
-            
             print "</tr>";
         }
         
-        // Erro 3 (tabela fechada) corrigido aqui
         print "</table>";
         
     } else {
-        // Erro 5 (lógica) corrigido aqui
         print "<p class='alert alert-danger'>Não encontrou nenhum resultado!</p>";
     }
     
