7bc0315990b441423abd6b59b1c42c48eee2638c
Willyan <willyanalvesgomes@hotmail.com>
2025-11-11 21:32:50 -0300

    refatora páginas de edição de cliente e funcionário, melhorando a estrutura dos formulários e a usabilidade

--- editar-cliente.php
+++ editar-cliente.php
@@ -1,51 +1,38 @@
-<link rel="stylesheet" href="css/estilo.css">
-<div class="container mt-4">
 <h1>Editar Cliente</h1>
 <?php
-    // 1. Busca os dados do CLIENTE específico
-    $sql = "SELECT * FROM cliente
-            WHERE id_cliente=".$_REQUEST['id_cliente'];
-
+    $sql = "SELECT * FROM cliente WHERE id_cliente=".$_REQUEST['id_cliente'];
     $res = $conn->query($sql);
     $row = $res->fetch_object();
 ?>
 <form action="?page=salvar-cliente" method="POST">
     <input type="hidden" name="acao" value="editar">
-    
     <input type="hidden" name="id_cliente" value="<?php print $row->id_cliente;?>">
     
     <div class="mb-3">
-        <label>Nome
-            <input type="text" name="nome_cliente" class="form-control" value="<?php print $row->nome_cliente;?>">
-        </label>
+        <label for="nome" class="form-label">Nome</label>
+        <input type="text" name="nome_cliente" id="nome" class="form-control" value="<?php print $row->nome_cliente;?>">
     </div>
     <div class="mb-3">
-        <label>Email
-            <input type="email" name="email_cliente" class="form-control" value="<?php print $row->email_cliente;?>">
-        </label>
+        <label for="email" class="form-label">Email</label>
+        <input type="email" name="email_cliente" id="email" class="form-control" value="<?php print $row->email_cliente;?>">
     </div>
     <div class="mb-3">
-        <label>CPF
-            <input type="text" name="cpf_cliente" class="form-control" value="<?php print $row->cpf_cliente;?>">
-        </label>
+        <label for="cpf" class="form-label">CPF</label>
+        <input type="text" name="cpf_cliente" id="cpf" class="form-control" value="<?php print $row->cpf_cliente;?>">
     </div>
     <div class="mb-3">
-        <label>Data de Nascimento
-            <input type="date" name="dt_nasc_cliente" class="form-control" value="<?php print $row->dt_nasc_cliente;?>">
-        </label>
+        <label for="data" class="form-label">Data de Nascimento</label>
+        <input type="date" name="dt_nasc_cliente" id="data" class="form-control" value="<?php print $row->dt_nasc_cliente;?>">
     </div>
     <div class="mb-3">
-        <label>Telefone
-            <input type="text" name="fone_cliente" class="form-control" value="<?php print $row->fone_cliente;?>">
-        </label>
+        <label for="fone" class="form-label">Telefone</label>
+        <input type="text" name="fone_cliente" id="fone" class="form-control" value="<?php print $row->fone_cliente;?>">
     </div>
     <div class="mb-3">
-        <label>Endereço
-            <input type="text" name="endereco_cliente" class="form-control" value="<?php print $row->endereco_cliente;?>">
-        </label>
+        <label for="endereco" class="form-label">Endereço</label>
+        <input type="text" name="endereco_cliente" id="endereco" class="form-control" value="<?php print $row->endereco_cliente;?>">
     </div>
-    <div>
+    <div class="mb-3">
         <button type="submit" class="btn btn-primary">Salvar Alterações</button>
     </div>
 </form>
-</div>
--- editar-funcionario.php
+++ editar-funcionario.php
@@ -1,39 +1,30 @@
-<link rel="stylesheet" href="css/estilo.css">
-<div class="container mt-4">
 <h1>Editar Funcionário</h1>
 <?php
-    
-    $sql = "SELECT * FROM funcionario
-    WHERE id_funcionario=".$_REQUEST['id_funcionario'];
-
+    $sql = "SELECT * FROM funcionario WHERE id_funcionario=".$_REQUEST['id_funcionario'];
     $res = $conn->query($sql);
     $row = $res->fetch_object();
 ?>
 <form action="?page=salvar-funcionario" method="POST">
     <input type="hidden" name="acao" value="editar">
     <input type="hidden" name="id_funcionario" value="<?php print $row->id_funcionario;?>">
+
     <div class="mb-3">
-        <label>Nome
-            <input type="text" name="nome_funcionario" class="form-control" value="<?php print $row->nome_funcionario;?>">
-        </label>
+        <label for="nome" class="form-label">Nome</label>
+        <input type="text" name="nome_funcionario" id="nome" class="form-control" value="<?php print $row->nome_funcionario;?>">
     </div>
     <div class="mb-3">
-        <label>Email
-            <input type="email" name="email_funcionario" class="form-control" value="<?php print $row->email_funcionario;?>">
-        </label>
+        <label for="email" class="form-label">Email</label>
+        <input type="email" name="email_funcionario" id="email" class="form-control" value="<?php print $row->email_funcionario;?>">
     </div>
     <div class="mb-3">
-        <label>CPF
-            <input type="text" name="cpf_funcionario" class="form-control" value="<?php print $row->cpf_funcionario;?>">
-        </label>
+        <label for="cpf" class="form-label">CPF</label>
+        <input type="text" name="cpf_funcionario" id="cpf" class="form-control" value="<?php print $row->cpf_funcionario;?>">
     </div>
     <div class="mb-3">
-        <label>Telefone
-            <input type="text" name="fone_funcionario" class="form-control" value="<?php print $row->fone_funcionario;?>">
-        </label>
+        <label for="fone" class="form-label">Telefone</label>
+        <input type="text" name="fone_funcionario" id="fone" class="form-control" value="<?php print $row->fone_funcionario;?>">
     </div>
-    <div>
-        <button type="submit" class="btn btn-primary">Enviar</button>
+    <div class="mb-3">
+        <button type="submit" class="btn btn-primary">Salvar Alterações</button>
     </div>
 </form>
-</div>
