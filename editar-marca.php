aaf397ee28a270e47034a5dc899ca866d808df25
Willyan <willyanalvesgomes@hotmail.com>
2025-11-11 21:27:35 -0300

    refatora página de edição de marca, melhorando a estrutura do formulário e ajustando mensagens de sucesso e erro

--- editar-marca.php
+++ editar-marca.php
@@ -1,24 +1,19 @@
-<link rel="stylesheet" href="css/estilo.css">
-<div class="container mt-4">
 <h1>Editar Marca</h1>
 <?php
-    
-    $sql = "SELECT * FROM marca
-    WHERE id_marca=".$_REQUEST['id_marca'];
-
+    $sql = "SELECT * FROM marca WHERE id_marca = ".$_REQUEST['id_marca'];
     $res = $conn->query($sql);
     $row = $res->fetch_object();
 ?>
 <form action="?page=salvar-marca" method="POST">
     <input type="hidden" name="acao" value="editar">
-    <input type="hidden" name="id_marca" value="<?php print $row->id_marca;?>">
+    <input type="hidden" name="id_marca" value="<?php print $row->id_marca; ?>">
+    
     <div class="mb-3">
-        <label>Nome
-            <input type="text" name="nome_marca" class="form-control" value="<?php print $row->nome_marca;?>">
-        </label>
+        <label for="nome" class="form-label">Nome da Marca</label>
+        <input type="text" name="nome_marca" id="nome" class="form-control" value="<?php print $row->nome_marca; ?>">
     </div>
-    <div>
-        <button type="submit" class="btn btn-primary">Enviar</button>
+    
+    <div class="mb-3">
+        <button type="submit" class="btn btn-primary">Salvar Alterações</button>
     </div>
 </form>
-</div>
--- salvar-marca.php
+++ salvar-marca.php
@@ -1,57 +1,46 @@
 <?php
-switch ($_POST["acao"]) {
+switch ($_REQUEST["acao"]) {
     case "cadastrar":
-        // 1. Pegar o nome da marca que veio do formulário
         $nome_marca = $_POST["nome_marca"];
-
-        // 2. Montar o SQL de INSERT
         $sql = "INSERT INTO marca (nome_marca) VALUES ('{$nome_marca}')";
-
-        // 3. Executar
         $res = $conn->query($sql);
 
-        // 4. Verificar e redirecionar
         if ($res == true) {
             print "<script>alert('Marca cadastrada com sucesso!');</script>";
-            print "<script>location.href='?page=cadastrar-modelo';</script>";
+            print "<script>location.href='?page=listar-marca';</script>"; 
         } else {
             print "<script>alert('Não foi possível cadastrar a marca.');</script>";
-            print "<script>location.href='?page=cadastrar-marca';</script>";
+            print "<script>location.href='?page=listar-marca';</script>";
         }
         break;
-
-    case "editar":
+        
+    case 'editar':
         $id = $_POST["id_marca"];
         $nome = $_POST["nome_marca"];
 
-        $sql = "UPDATE marca SET 
-                    nome_marca = '{$nome}'
-                WHERE 
-                    id_marca = {$id}";
-
+        $sql = "UPDATE marca SET nome_marca = '{$nome}' WHERE id_marca = {$id}";
         $res = $conn->query($sql);
 
         if ($res == true) {
-            print "<script>alert('Marca atualizada com sucesso!');</script>";
+            print "<script>alert('Marca editada com sucesso!');</script>";
             print "<script>location.href='?page=listar-marca';</script>";
         } else {
-            print "<script>alert('Não foi possível atualizar a marca.');</script>";
+            print "<script>alert('Não foi possível editar a marca.');</script>";
             print "<script>location.href='?page=listar-marca';</script>";
         }
         break;
-
-    case "excluir":
-        $id = $_POST["id_marca"];
-
+    
+    case 'excluir':
+        $id = $_REQUEST["id_marca"];
+     
         $sql = "DELETE FROM marca WHERE id_marca = {$id}";
-
         $res = $conn->query($sql);
 
         if ($res == true) {
             print "<script>alert('Marca excluída com sucesso!');</script>";
             print "<script>location.href='?page=listar-marca';</script>";
         } else {
-            print "<script>alert('Não foi possível excluir a marca.');</script>";
+            print "<script>alert('Não foi possível excluir. Verifique se existem modelos associados a esta marca.');</script>";
             print "<script>location.href='?page=listar-marca';</script>";
         }
         break;
